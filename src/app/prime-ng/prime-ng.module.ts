114ccb05932c1ab71bf64855be51d1c8f1629ca7
Marcos <marcof23@hotmail.com>
2023-07-07 13:38:57 -0300

    Antes de agregar el npm install @angular/animations --save

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MenuModule } from 'primeng/menu';
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MenuModule
+  ],
+  imports: []
+})
+
+export class PrimeNgModule { }
